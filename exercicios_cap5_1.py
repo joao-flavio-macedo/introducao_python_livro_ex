20b667ef241e837de25048899eb8ed0f7b493c6b
João Ananias <jflaviomacedo@outlook.com>
2022-06-01 21:13:44 -0300

    lista de exercicios capitulo 5

---
+++ exercicios_cap5_1.py
@@ -0,0 +1,25 @@
+# Modifique o programa para exibir os números de 1 a 100
+
+# Modifique o programa para exibir os número de 50 a 100
+
+# Faça um programa para escrever a contagem regressiva do lançamento de um foguete
+# O programa deve imprimir 10,9,8...1,0 e fogo! na tela.
+
+# Modifique o programa anterior para imprimir de 1 até o número digitado pelo usuário
+# mas dessa vez, apenas os números ímpares
+
+# Reescreva o programa anterior para escrever os 10 primeiros múltiplos de 3
+
+# Altere o programa anterior para exibir os resultados no mesmo formato de uma tabuada
+
+# Modifique o programa anterior de forma que o usuário também digite o início e o fim da tabuada
+# em vez de começar com 1 e 10
+
+# Escreva um programa que leia dois numeros. Imprima o resultado da multiplicação do primeiro pelo segundo. Utilize apenas os operadores
+# de soma e subtraçao para calcular o resultado. Lembre-se de que podemos entender a multiplicação de dois números como somas sucessivas
+# de um deles. Assim, 4x5 = 5+5+5+5 = 4+4+4+4+4
+
+# Escreva um programa que leia dois números. Imprima a divisão inteira do primeiro pelo segundo, assim como o resto da divisão.
+# inteira do primeiro pelo segundo, assim como o resto da divisão. Utilize apenas os operadores de soma e subtração para calcular 
+# o resultado. Lembre-se de que podemos entender o quociente da divisão de dois números como a quantidade de vezes que podemos
+# retirar o divisor do dividendo. Logo, 20 +4 =5, uma vez que podemos subtrair 4 cinco vezes de 20.
