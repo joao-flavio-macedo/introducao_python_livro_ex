b56910e0a6b42371937ff28b05428105423d8167
João Ananias <jflaviomacedo@outlook.com>
2022-06-03 15:38:48 -0300

    Atualização capítulo 5

---
+++ exercicios_cap5.py
@@ -0,0 +1,49 @@
+# Modifique o programa para exibir os números de 1 a 100
+
+# Modifique o programa para exibir os número de 50 a 100
+
+# Faça um programa para escrever a contagem regressiva do lançamento de um foguete
+# O programa deve imprimir 10,9,8...1,0 e fogo! na tela.
+
+# Modifique o programa anterior para imprimir de 1 até o número digitado pelo usuário
+# mas dessa vez, apenas os números ímpares
+
+# Reescreva o programa anterior para escrever os 10 primeiros múltiplos de 3
+
+# Altere o programa anterior para exibir os resultados no mesmo formato de uma tabuada
+
+# Modifique o programa anterior de forma que o usuário também digite o início e o fim da tabuada
+# em vez de começar com 1 e 10
+
+# Escreva um programa que leia dois numeros. Imprima o resultado da multiplicação do primeiro pelo segundo. Utilize apenas os operadores
+# de soma e subtraçao para calcular o resultado. Lembre-se de que podemos entender a multiplicação de dois números como somas sucessivas
+# de um deles. Assim, 4x5 = 5+5+5+5 = 4+4+4+4+4
+
+# Escreva um programa que leia dois números. Imprima a divisão inteira do primeiro pelo segundo, assim como o resto da divisão.
+# inteira do primeiro pelo segundo, assim como o resto da divisão. Utilize apenas os operadores de soma e subtração para calcular 
+# o resultado. Lembre-se de que podemos entender o quociente da divisão de dois números como a quantidade de vezes que podemos
+# retirar o divisor do dividendo. Logo, 20 +4 =5, uma vez que podemos subtrair 4 cinco vezes de 20.
+
+# Modifique o programa anterior para que aceite respostas com letras maiusculas e minusculas em todas as questões
+
+# Escreva um programa que pergunte o depósito inicial e a taxa de juros de uma poupança. Exiba os valores mês a mês para os 24
+# primeiros meses. Escreva o total ganho com juros no período.
+
+# Altere o programa anterior de forma a perguntar também o valor depositado mensalmente. Esse valor será depositado no início de
+# cada mês, e você deve considerá-lo para o cálculo de juros do mês seguinte.
+
+# Escreva um programa que pergunte o valor inicial de uma dívida e o juro mensal. Pergunte também o valor mensal que será pago Imprima
+# o número de meses para que a dívida seja paga, o total pago e o total de juros pago.
+
+# Escreva um programa que leia numeros inteiros do teclado. O programa deve ler os numeros até que o usuário digite 0 (zero). No final da execução
+# exiba a quantidade de números digitados, assim como a soma e a média aritmética.
+
+# Escreva um programa para controlar uma pequena maquina registradora. Você deve solicitar ao usuário que digite o código do produto e a quantidade
+# comprada. Utilize a tabela de códigos a seguir para obter o preço de cada produto:
+
+#Codigo Preço
+# 1     0,50
+# 2     1,00
+# 3     4,00
+# 4     7,00
+# 5     8,00
