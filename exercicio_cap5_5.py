553f1035fe94c946ce35053363d3dc7093ec4d36
joao-flavio-macedo <jflaviomacedo@outlook.com>
2022-06-06 22:49:06 +0100

    atualização dos exercicios 5

---
+++ exercicio_cap5_5.py
@@ -0,0 +1,14 @@
+
+#count = 1
+#numero = int(input("Digite um número maior do que UM:"))
+#while count <= numero:
+#    if count % 2 == 1:
+#        print(count)
+#    count += 1
+
+# Reescreva o programa anterior para escrever os 10 primeiros múltiplos de 3
+
+count = 1
+while count <= 10:
+    print(count*3)
+    count += 1
