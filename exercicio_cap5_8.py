077d124f75f215c0231201632fa0ebdbbfa6accb
joao-flavio-macedo <jflaviomacedo@outlook.com>
2022-06-07 00:47:21 +0100

    atualização dos exercicios 5.8

--- exercicio_cap5_8.py
+++ exercicio_cap5_8.py
@@ -2,17 +2,6 @@
 # de soma e subtraçao para calcular o resultado. Lembre-se de que podemos entender a multiplicação de dois números como somas sucessivas
 # de um deles. Assim, 4x5 = 5+5+5+5 = 4+4+4+4+4
 
-# primeiro_numero = int(input("Informe o primeiro número:"))
-# segundo_numero = int(input("Informe o segundo número:"))
-# count =1
-# print(f"{primeiro_numero} x {segundo_numero} =")
-# while count <= primeiro_numero:
-#     print(f"{segundo_numero}")
-#     count += 1
-
-# for dia in ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo']:
-#     print(dia)
-
 primeiro_numero = int(input("Informe o primeiro número:"))
 segundo_numero = int(input("Informe o segundo número:"))
 numeros = []
