e68a53b039e59e5cee3b09a4b16473c9fe6ffcb1
joao-flavio-macedo <jflaviomacedo@outlook.com>
2022-06-07 01:01:12 +0100

    atualização dos exercicios 5.8

---
+++ exercicio_cap5_9.py
@@ -0,0 +1,11 @@
+# Escreva um programa que leia dois números. Imprima a divisão inteira do primeiro pelo segundo, assim como o resto da divisão.
+# inteira do primeiro pelo segundo, assim como o resto da divisão. Utilize apenas os operadores de soma e subtração para calcular 
+# o resultado. Lembre-se de que podemos entender o quociente da divisão de dois números como a quantidade de vezes que podemos
+# retirar o divisor do dividendo. Logo, 20/4 =5, uma vez que podemos subtrair 4 cinco vezes de 20.
+primeiro_numero = int(input("Informe o primeiro número:"))
+segundo_numero = int(input("Informe o segundo número:"))
+numeros = []
+for count in range(primeiro_numero):
+    numeros += [count]
+
+print(f"A divisão é a subtração sucessiva de: {numeros}")
