367fea23ba80962138aaba73657f6c098c8efe11
joao-flavio-macedo <jflaviomacedo@outlook.com>
2022-06-16 01:42:58 +0100

    inserindo exercicios

---
+++ exercicio_cap5_10.3.py
@@ -0,0 +1,40 @@
+# Escreva um programa que pergunte o depósito inicial e a taxa de juros de uma poupança.
+# exiba os valores mês a mês para os 24 primeiros meses. Escreva o total ganho com juros no período
+
+# Altere o programa anterior de forma a perguntar também o valor depositado mensalmente. Esse valor será depositado no início de cada mês, e você
+#  deve considerá-lo para o cálculo de juros do mês seguinte. 
+
+# Escreva um programa que pergunte o valor inicial de uma dívida e o juro mensal. Pergunte também o valor mensal que será pago. Imprima o número de meses
+# para que a dívida seja paga, o total pago e o total de juros pago.
+
+# Escreva um programa que leia números inteiros do teclado. O programa deve ler os números até que o usuário digite 0(zero). No final da execução, exiba a 
+# quantidade de números digitados, assim como a soma e a média aritmética.
+
+# Escreva um programa para controlar uma pequena máquina registradora. Você deve solicitar ao usuário que digite o código do produto e a quantidade comprada. Utilize
+# a tabela de códigos a seguir para obter o preço de cada produto:
+
+#Codigo
+#1 0,50
+#2 1,00
+#3 4,00
+#4 7,00
+#5 8,00
+
+# Seu programa deve exibir o total de compras depois que o usuário digitar 0. Qualquer outro código deve gerar a mensagem de erro "Código inválido"
+
+# Execute o programa 5.1 para os seguintes valores: 501, 745, 384, 2, 7 e 1.
+
+# O que acontece se digitarmos 0(zero) no valor a pagar ?
+
+# Modifique o programa para também trabalhar com notas de R$ 100.
+
+# Modifique o programa para aceitar valores decimais, ou seja, também contar moedas de 0,01 0,02 0,05 0,10 e 0,50
+
+# O que acontece se digitarmos 0,001 no programa anterior? Caso ele não funcione, altere-o de forma a corrigir o problema.
+
+# Reescreva o programa 5.1 de forma a continuar executando até que o valor digitado seja 0. Utilize repetições aninhadas.
+
+# Escreva um programa que exiba uma lista de opções(menu): adição, subtração, divisão, multiplicação e sair. Imprima a tabuada da operaçao escolhida
+# Repita até que a opção saída seja escolhida
+
+#
